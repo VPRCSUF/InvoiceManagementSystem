ed94bb4950b18d55e0b75638ec35deeb957991a5
Vincent <royrosevin@csu.fullerton.edu>
2023-12-14 10:55:50 -0600

    Added install and warehouse functions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
+void install() {
+
+    std::cout << "install function";
+    
+}
+
+void warehouse() {
+
+    std::cout << "warehouse function";
+
+}
+
+int main(int argc, char const *argv[]) {
+
     int user;
     
-    std::cout << "\nAre you\n1. Warehouse\n\n2.Install\n";
+    std::cout << std::endl << "1. Install\n" << std::endl
+              << "2. Warehouse\n" << std::endl;
+
     std::cin >> user;
+
+    if (user > 2) {
+
+        std::cout << "Invalid input.";
+
+    } else if (user == 1) {
+
+        install();
+
+    } else {
+
+        warehouse();
+
+    }
+
     return 0;
 }
